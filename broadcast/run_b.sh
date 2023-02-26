c7b41257bbf53f65083c4f7ed333d61e9494cefa
Corey Alexander <coreyja@gmail.com>
2023-02-26 14:48:20 -0500

    Push up failing test for 3b

---
+++ broadcast/run_b.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+cargo build
+java -jar ~/maelstrom/lib/maelstrom.jar test \
+  -w broadcast \
+  --bin ~/Projects/gossip-glomers/target/debug/broadcast \
+  --node-count 5 \
+  --time-limit 20 \
+  --rate 10
