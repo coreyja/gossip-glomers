06d1f8f353bb27a8005e19439c46f2fa74f3f595
Corey Alexander <coreyja@gmail.com>
2023-02-26 13:21:51 -0500

    Refactor to have a Node, and store ids better. Structure is looking better

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,10 +1,34 @@
 use color_eyre::eyre::Result;
 use serde::{Deserialize, Serialize};
 
-use std::io::BufRead;
+#[derive(Debug)]
+struct Node {
+    id: String,
+    peers: Vec<String>,
+
+    next_msg_id: MsgId,
+}
 
 fn main() -> Result<()> {
     let stdin = std::io::stdin();
+
+    // Init the node BEFORE we start the loop. We know the first message MUST be an init message
+    let mut buffer = String::new();
+    stdin.read_line(&mut buffer)?;
+    let m = serde_json::from_str::<Message<RequestBody>>(&buffer)?;
+
+    let Message { body: RequestBody::Init(Init{node_id, node_ids, ..}), .. } = &m else {
+        panic!("Expected init message, got {:?}", m);
+    };
+
+    let mut node = Node {
+        id: node_id.clone(),
+        peers: node_ids.clone(),
+
+        next_msg_id: 0,
+    };
+
+    node.handle_message(m)?;
 
     loop {
         let mut buffer = String::new();
@@ -12,32 +36,56 @@ fn main() -> Result<()> {
 
         if bytes != 0 && !buffer.is_empty() {
             eprintln!("Received: {}", buffer);
-            // if m.body.t != "echo" {
-            //     panic!("Unexpected message type: {:?}", m.body.t);
-            // }
 
-            let m = serde_json::from_str::<Message>(&buffer)?;
+            let m = serde_json::from_str::<Message<RequestBody>>(&buffer)?;
 
-            let resp = match &m.body {
-                Body::Echo(e) => Some(e.respond(m.src, m.dest)),
-                Body::Init(e) => Some(e.respond(m.src, m.dest)),
-                Body::InitResponse(_) => None,
-                _ => panic!("Unexpected message type: {:?}", m.body),
-            };
-
-            if let Some(resp) = resp {
-                let output = serde_json::to_string(&resp)?;
-
-                println!("{output}");
-            }
+            node.handle_message(m)?;
         }
+    }
+}
+
+impl Node {
+    fn create_response(&mut self, m: Message<RequestBody>) -> Message<ResponseBody> {
+        let body = self.create_response_body(&m);
+
+        Message {
+            body,
+            dest: m.src.clone(),
+            src: self.id.clone(),
+        }
+    }
+
+    fn create_response_body(&mut self, m: &Message<RequestBody>) -> ResponseBody {
+        let new_msg_id = self.generate_msg_id();
+
+        match &m.body {
+            RequestBody::Echo(e) => e.response(new_msg_id),
+            RequestBody::Init(e) => e.response(new_msg_id),
+        }
+    }
+
+    fn handle_message(&mut self, m: Message<RequestBody>) -> Result<()> {
+        let resp = self.create_response(m);
+
+        let output = serde_json::to_string(&resp)?;
+
+        println!("{output}");
+
+        Ok(())
+    }
+
+    fn generate_msg_id(&mut self) -> MsgId {
+        let next = self.next_msg_id;
+        self.next_msg_id += 1;
+
+        next
     }
 }
 
 type MsgId = u64;
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
-struct Message {
+struct Message<Body> {
     body: Body,
     dest: String,
     src: String,
@@ -45,23 +93,22 @@ struct Message {
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
-enum Body {
+enum RequestBody {
     #[serde(rename = "echo")]
     Echo(Echo),
-    #[serde(rename = "echo_ok")]
-    EchoResponse(EchoResponse),
     #[serde(rename = "init")]
     Init(Init),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+enum ResponseBody {
+    #[serde(rename = "echo_ok")]
+    EchoResponse(EchoResponse),
     #[serde(rename = "error")]
     ErrorMsg(ErrorMsg),
     #[serde(rename = "init_ok")]
     InitResponse(InitResponse),
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-struct NotFullMessageBody {
-    #[serde(rename = "type")]
-    t: String,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
@@ -92,18 +139,12 @@ struct ErrorMsg {
 }
 
 impl Echo {
-    fn respond(&self, dest: String, src: String) -> Message {
-        let resp = EchoResponse {
-            msg_id: self.msg_id + 1,
+    fn response(&self, new_msg_id: MsgId) -> ResponseBody {
+        ResponseBody::EchoResponse(EchoResponse {
+            msg_id: new_msg_id,
             echo: self.echo.clone(),
             in_reply_to: self.msg_id,
-        };
-
-        Message {
-            body: Body::EchoResponse(resp),
-            dest,
-            src,
-        }
+        })
     }
 }
 
@@ -114,16 +155,10 @@ struct InitResponse {
 }
 
 impl Init {
-    fn respond(&self, dest: String, src: String) -> Message {
-        let resp = InitResponse {
-            msg_id: self.msg_id + 1,
+    fn response(&self, new_msg_id: MsgId) -> ResponseBody {
+        ResponseBody::InitResponse(InitResponse {
+            msg_id: new_msg_id,
             in_reply_to: self.msg_id,
-        };
-
-        Message {
-            body: Body::InitResponse(resp),
-            dest,
-            src,
-        }
+        })
     }
 }
