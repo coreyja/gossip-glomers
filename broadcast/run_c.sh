b06e73d8a545aae24222f06d42f708557e0fc0b6
Corey Alexander <coreyja@gmail.com>
2023-02-26 19:16:02 -0500

    Create fault tolerant test case

---
+++ broadcast/run_c.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+cargo build
+java -jar ~/maelstrom/lib/maelstrom.jar test \
+  -w broadcast \
+  --bin ~/Projects/gossip-glomers/target/debug/broadcast \
+  --node-count 5 \
+  --time-limit 20 \
+  --rate 10 \
+   --nemesis partition
